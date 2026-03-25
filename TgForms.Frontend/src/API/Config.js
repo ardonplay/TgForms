fdfad80a9ddc493359e7bd097b6aeea403fd395d
Vladimir Moshchuk <ardonplay@gmail.com>
2026-03-25 09:20:48 +0300

    Update Config.js

--- TgForms.Frontend/src/API/Config.js
+++ TgForms.Frontend/src/API/Config.js
@@ -1,5 +1,5 @@
 export default class Config {
-  static BaseUrl ="https://tgforms-bot.space"
+  static BaseUrl ="https://6690-45-92-174-106.ngrok-free.app/"
   //static BaseUrl ="https://9836-37-110-215-121.ngrok-free.app"
   // static BaseUrl ="http://localhost:59312"
 }
